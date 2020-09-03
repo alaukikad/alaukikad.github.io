b31c4f025a1cb1aa59d102a638497b2414cbd89c
Alaukika Diwanji <alaukikadiwanji@Alaukikas-MacBook-Pro.local>
2020-09-02 19:51:51 -0500

    Updates

---
+++ precache-manifest.45244ebc741db0f0142b543b779d28e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1af43a1973757d8a6a4f3a1c1565c08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9e1a6622add9ca5b29be",
+    "url": "/static/css/main.f60eae6a.chunk.css"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/js/2.0789156f.chunk.js"
+  },
+  {
+    "revision": "b25e0c44d0b24745f7d62e2ea6be31e5",
+    "url": "/static/js/2.0789156f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e1a6622add9ca5b29be",
+    "url": "/static/js/main.8b0fadfe.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
